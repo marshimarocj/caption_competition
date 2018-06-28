388066d0c371ba9e997439b5499d00784e854e14
Jia Chen <jiac@t470>
2018-06-28 14:21:35 -0400

    sync

--- expr/prepare_rank_cfg.py
+++ expr/prepare_rank_cfg.py
@@ -77,6 +77,68 @@ def prepare_ceve():
     json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
 
 
+def prepare_ceve_score():
+  root_dir = '/mnt/data1/jiac/trecvid2018/rank' # neptune
+  split_dir = os.path.join(root_dir, 'split')
+  label_dir = os.path.join(root_dir, 'label')
+  word_file = os.path.join(root_dir, 'annotation', 'int2word.pkl')
+  embed_file = os.path.join(root_dir, 'annotation', 'E.word2vec.npy') 
+  out_dir = os.path.join(root_dir, 'ceve_expr')
+  splits = ['trn', 'val', 'tst']
+
+  ft_names = [
+    'i3d',
+    'resnet200',
+  ]
+
+  params = {
+    'num_epoch': 50,
+
+    'alpha': 0.5,
+    'num_neg': 32,
+    'l2norm': True,
+    'dim_ft': 1024 + 2048,
+    'dim_joint_embed': 300,
+
+    'max_words_in_caption': 30,
+    'window_sizes': [1, 2, 3],
+    'num_filters': [100, 100, 100],
+    'pool': 'max',
+  }
+
+  outprefix = '%s.%d.%s.%s.%.1f.score'%(
+    os.path.join(out_dir, '_'.join(ft_names)), 
+    params['dim_joint_embed'], '_'.join([str(d) for d in params['window_sizes']]), 
+    params['pool'], params['alpha'])
+
+  model_cfg = rank_model.ceve_score.gen_cfg(**params)
+
+  model_cfg_file = '%s.model.json'%outprefix
+  model_cfg.save(model_cfg_file)
+
+  output_dir = outprefix
+  path_cfg = {
+    'trn_ftfiles': [os.path.join(root_dir, 'mp_feature', ft_name, 'trn_ft.npy') for ft_name in ft_names],
+    'val_ftfiles': [os.path.join(root_dir, 'mp_feature', ft_name, 'val_ft.2.npy') for ft_name in ft_names],
+    'tst_ftfiles': [],
+    'val_label_file': os.path.join(label_dir, '17.set.2.gt'),
+    'trn_annotation_file': os.path.join(split_dir, 'trn_id_caption_mask.pkl'),
+    'val_annotation_file': os.path.join(split_dir, 'val_id_caption_mask.A.pkl'),
+    'tst_annotation_file': '',
+    'vid_file': os.path.join(split_dir, 'trn_videoids.npy'),
+    'word_file': word_file,
+    'embed_file': embed_file,
+    'output_dir': output_dir,
+  }
+  path_cfg_file = '%s.path.json'%outprefix
+
+  if not os.path.exists(path_cfg['output_dir']):
+    os.mkdir(path_cfg['output_dir'])
+
+  with open(path_cfg_file, 'w') as fout:
+    json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
+
+
 def prepare_rnnve():
   root_dir = '/data1/jiac/trecvid2018/rank' # uranus
   split_dir = os.path.join(root_dir, 'split')
@@ -142,4 +204,5 @@ def prepare_rnnve():
 
 if __name__ == '__main__':
   # prepare_ceve()
-  prepare_rnnve()
+  # prepare_rnnve()
+  prepare_ceve_score()
