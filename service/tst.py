25dd1680b488bcfcb94ef210ce140f85a539b9ac
Jia Chen <jiac@t470>
2018-06-28 14:44:42 -0400

    sync

--- service/tst.py
+++ service/tst.py
@@ -15,7 +15,8 @@ def tst_post():
   # url = 'localhost:8888'
   # url = '18.216.242.151:8888'
   # url = 'aladdin1.inf.cs.cmu.edu:8888'
-  url = '127.0.0.1:8888'
+  # url = '127.0.0.1:8888'
+  url = '172.17.0.1:8888'
 
   data = [{'pred': 'a man is playing', 'vid': 999, 'id': 0}]
   r = requests.post('http://' + url + '/cider', json=data)
