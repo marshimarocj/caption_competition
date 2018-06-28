21b44d6a3e2392d9215e2c621c152b607277091f
Jia Chen <jiac@t470>
2018-06-27 20:13:22 -0400

    sync

--- rank_model/rnnve.py
+++ rank_model/rnnve.py
@@ -167,6 +167,7 @@ class Model(framework.model.module.AbstractModel):
       mask = tf.expand_dims(tf.to_float(mask), 2)
       caption_embed = tf.nn.conv1d(caption_embed, tf.expand_dims(self.caption_pca_W, 0), 1, 'VALID')
       caption_embed = tf.nn.tanh(caption_embed)
+      print caption_embed.get_shape()
       if self._config.pool == 'mean':
         caption_embed = tf.reduce_sum(caption_embed*mask, 1) / tf.reduce_sum(mask, 1, True)
       else:
@@ -176,6 +177,7 @@ class Model(framework.model.module.AbstractModel):
         caption_embed = tf.reduce_max(caption_embed, 1)
       if self._config.l2norm:
         caption_embed = tf.nn.l2_normalize(caption_embed, 1)
+      print caption_embed.get_shape()
 
       ft_embed = tf.nn.xw_plus_b(in_ops[self.InKey.FT], self.ft_pca_W, self.ft_pca_B)
       ft_embed = tf.nn.tanh(ft_embed)
