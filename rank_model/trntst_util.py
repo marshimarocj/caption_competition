8a0ba91ec2e0ca32b486071eb21f166844d57fff
Jia Chen <jiac@t470>
2018-06-28 11:01:55 -0400

    sync

--- rank_model/trntst_util.py
+++ rank_model/trntst_util.py
@@ -1,9 +1,13 @@
 import cPickle
 import random
+import requests
+import threading
+from Queue import Queue
 
 import numpy as np
 
 import framework.model.trntst
+import framework.util.caption.utility
 
 
 class PathCfg(framework.model.trntst.PathCfg):
@@ -25,6 +29,12 @@ class PathCfg(framework.model.trntst.PathCfg):
 
     # automatically generated paths
     self.log_file = ''
+
+
+class ScorePathCfg(PathCfg):
+  def __init__(self):
+    super(ScorePathCfg, self).__init__()
+    self.trn_vid_file = ''
 
 
 class TrnTst(framework.model.trntst.TrnTst):
@@ -72,6 +82,17 @@ class TrnTst(framework.model.trntst.TrnTst):
       sims.append(sim)
     sims = np.concatenate(sims, 0)
     np.save(predict_file, sims)
+
+
+class ScoreTrnTst(TrnTst):
+  def _construct_feed_dict_in_trn(self, data):
+    return {
+      self.model.inputs[self.model.InKey.FT]: data['fts'],
+      self.model.inputs[self.model.InKey.CAPTIONID]: data['captionids'],
+      self.model.inputs[self.model.InKey.CAPTION_MASK]: data['caption_masks'],
+      self.model.inputs[self.model.InKey.DELTA]: data['deltas'],
+      self.model.inputs[self.model.InKey.IS_TRN]: True,
+    }
 
 
 class TrnReader(framework.model.data.Reader):
@@ -212,3 +233,128 @@ class TstReader(framework.model.data.Reader):
         'captionids': self.captionids,
         'caption_masks': self.caption_masks,
       }
+
+
+class ScoreTrnReader(framework.model.data.Reader):
+  def __init__(self, num_neg, ft_files, annotation_file, vid_file, word_file):
+    self.num_neg = num_neg
+    self.fts = np.empty(0)
+    self.ft_idxs = np.empty(0)
+    self.captionids = np.empty(0)
+    self.caption_masks = np.empty(0)
+    self.idxs = []
+    self.vids = np.empty(0)
+    self.num_caption = 0
+
+    fts = []
+    for ft_file in ft_files:
+      ft = np.load(ft_file)
+      fts.append(ft)
+    self.fts = np.concatenate(tuple(fts), axis=1)
+    self.fts = self.fts.astype(np.float32)
+
+    data = cPickle.load(file(annotation_file))
+    self.ft_idxs = data[0]
+    self.captionids = data[1]
+    self.caption_masks = data[2]
+
+    self.vids = np.load(vid_file)
+    self.int2str = framework.util.caption.utility.CaptionInt2str(word_file)
+
+    self.num_caption = self.captionids.shape[0]
+    self.idxs = range(self.num_caption)
+
+  def num_record(self):
+    return self.num_caption
+
+  def reset(self):
+    random.shuffle(self.idxs)
+
+  def yield_trn_batch(self, batch_size):
+    for i in range(0, self.num_caption, batch_size):
+      pos_idxs = self.idxs[i:i+batch_size]
+      pos_ft_idxs = set(self.ft_idxs[pos_idxs].tolist())
+
+      pos_fts = self.fts[self.ft_idxs[pos_idxs]]
+      pos_captionids = self.captionids[pos_idxs]
+      pos_caption_masks = self.caption_masks[pos_idxs]
+      pos_vids = self.vids[self.ft_idxs[pos_idxs]]
+
+      idxs = range(self.num_caption)
+      random.shuffle(idxs)
+
+      neg_fts = []
+      neg_captionids= []
+      neg_caption_masks = []
+      for idx in idxs:
+        ft_idx = self.ft_idxs[idx]
+        if ft_idx not in pos_ft_idxs:
+          neg_fts.append(self.fts[ft_idx])
+          neg_captionids.append(self.captionids[idx])
+          neg_caption_masks.append(self.caption_masks[idx])
+          if len(neg_fts) == self.num_neg:
+            break
+      neg_fts = np.array(neg_fts, dtype=np.float32)
+      neg_captionids = np.array(neg_captionids, dtype=np.int32)
+      neg_caption_masks = np.array(neg_caption_masks, dtype=np.int32)
+
+      neg_captions = self.int2str(neg_captionids)
+      deltas = get_scores(neg_captions, pos_vids)
+
+      fts = np.concatenate([pos_fts, neg_fts], 0)
+      captionids = np.concatenate([pos_captionids, neg_captionids], 0)
+      caption_masks = np.concatenate([pos_caption_masks, neg_caption_masks], 0)
+
+      yield {
+        'fts': fts,
+        'captionids': captionids,
+        'caption_masks': caption_masks,
+        'deltas': deltas,
+      }
+
+
+def cider_scorer(data, q):
+  server_url = 'http://127.0.0.1:8888/cider'
+
+  r = requests.post(server_url, json=data)
+  data = json.loads(r.text)
+  q.put(data)
+
+
+def get_scores(neg_captions, pos_vids):
+  num_pos = pos_vids.shape[0]
+  num_neg = len(neg_captions)
+
+  vid2idx = {}
+  for i, vid in enumerate(pos_vids):
+    vid2idx[vid] = i
+
+  q = Queue()
+  for vid in pos_vids:
+    eval_data = []
+    for i, neg_caption in enumerate(neg_captions):
+      eval_data.append({
+        'pred': neg_caption,
+        'id': '%d_%d'%(vid, i),
+        'vid': vid,
+      })
+  worker = threading.Thread(
+    target=cider_scorer, args=(eval_data, q))
+  worker.start()
+
+  deltas = np.zeros((num_pos, num_neg), dtype=np.float32)
+  for t in range(num_pos):
+    data = q.get()
+    data = data['data']
+    for d in data:
+      score = d['score']
+      id = d['id']
+      fields = id.split('_')
+      vid = int(fields[0])
+      j = int(fields[1])
+      deltas[vid2idx[vid], j] = score
+
+  deltas = 1.0 - deltas
+  deltas = np.maximum(deltas, np.zeros(deltas.shape))
+
+  return deltas
