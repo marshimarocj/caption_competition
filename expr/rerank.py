830b95e369ff2ef2efc41778b8a695c2d35231df
Jia Chen <jiac@t470>
2018-06-28 11:31:55 -0400

    sync

--- expr/rerank.py
+++ expr/rerank.py
@@ -93,8 +93,8 @@ def eval_rerank():
   pred_files = [
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.mean.1.0', 'pred', 'val.A.npy'),
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.mean.1.0', 'pred', 'val.A.rerank.20.npy'),
-    os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.mean.0.5', 'pred', 'val.A.npy'),
-    os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.mean.0.5', 'pred', 'val.A.rerank.20.npy'),
+    os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.mean.0.5', 'pred', 'val.B.npy'),
+    os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.mean.0.5', 'pred', 'val.B.rerank.20.npy'),
   ]
   label_file = os.path.join(root_dir, 'label', '17.set.2.gt')
 
