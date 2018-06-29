1e871068f847120a84fd979581bc991f08646b69
Jia Chen <jiac@t470>
2018-06-29 18:00:40 -0400

    sync

--- expr/eval_rank.py
+++ expr/eval_rank.py
@@ -73,10 +73,10 @@ def predict_eval_trecvid17_B():
   root_dir = '/mnt/data1/jiac/trecvid2018/rank' # neptune
   ft_names = ['i3d', 'resnet200']
   ft_files = [os.path.join(root_dir, 'mp_feature', ft_name, 'val_ft.2.npy') for ft_name in ft_names]
-  annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.B.pkl')
-  out_name = 'val.B'
-  # annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.A.pkl')
-  # out_name = 'val.A'
+  # annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.B.pkl')
+  # out_name = 'val.B'
+  annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.A.pkl')
+  out_name = 'val.A'
   label_file = os.path.join(root_dir, 'label', '17.set.2.gt')
 
   # # expr_name = os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.mean.1.0')
@@ -95,7 +95,7 @@ def predict_eval_trecvid17_B():
   model_cfg_file = '%s.model.json'%expr_name
   path_cfg_file = '%s.path.json'%expr_name
   python_file = '../rank_driver/ceve_score.py'
-  gpuid = 0
+  gpuid = 1
 
   # expr_name = os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.300.150.gru.mean.0.5')
   # expr_name = os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.300.150.gru.max.0.5')
@@ -111,20 +111,20 @@ def predict_eval_trecvid17_B():
     ft_files=','.join(ft_files), annotation_file=annotation_file, out_name=out_name)
   p.wait()
 
-  vid2gt = {}
-  with open(label_file) as f:
-    for line in f:
-      line = line.strip()
-      data = line.split(' ')
-      vid = int(data[0])
-      gid = int(data[2])
-      vid2gt[vid] = gid
+  # vid2gt = {}
+  # with open(label_file) as f:
+  #   for line in f:
+  #     line = line.strip()
+  #     data = line.split(' ')
+  #     vid = int(data[0])
+  #     gid = int(data[2])
+  #     vid2gt[vid] = gid
 
-  predict_file = '%s/pred/%s.npy'%(expr_name, out_name)
-  predicts = np.load(predict_file)
-  mir_B = calc_mir(predicts, vid2gt)
+  # predict_file = '%s/pred/%s.npy'%(expr_name, out_name)
+  # predicts = np.load(predict_file)
+  # mir_B = calc_mir(predicts, vid2gt)
 
-  print best_epoch, mir_A, mir_B
+  # print best_epoch, mir_A, mir_B
 
 
 if __name__ == '__main__':
