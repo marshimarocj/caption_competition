bdaa933b04403e814b0776ce8b5c968304cf9c21
Jia Chen <jiac@t470>
2018-06-29 14:56:04 -0400

    sync

--- expr/prepare_gen_cfg.py
+++ expr/prepare_gen_cfg.py
@@ -6,6 +6,7 @@ sys.path.append('../')
 import numpy as np
 
 import gen_model.vevd
+import gen_model.self_critique
 
 
 '''func
@@ -83,5 +84,62 @@ def prepare_vevd():
   json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
 
 
+def prepare_self_critique():
+  root_dir = '/data1/jiac/trecvid2018/generation' # uranus
+  annotation_dir = os.path.join(root_dir, 'annotation')
+  split_dir = os.path.join(root_dir, 'split')
+  splits = ['trn', 'val', 'tst']
+  out_dir = os.path.join(root_dir, 'self_critique_expr')
+  model_spec = 'lstm'
+
+  ft_names = [
+    'i3d',
+    'resnet200',
+  ]
+
+  dim_fts, split_ftfiles = get_mean_ft_files(root_dir, ft_names, splits)
+
+  params = {
+    'num_epoch': 200,
+    'num_sample': 1,
+    'reward_metric': 'cider',
+    'alpha': 1.,
+
+    'num_step': 30,
+    'dim_input': 512,
+    'dim_hidden': 512,
+    'content_keepin_prob': 1.,
+    'cell_keepin_prob': 0.5,
+    'cell_keepout_prob': 0.5,
+    'dim_fts': dim_fts,
+  }
+
+  model_cfg = gen_model.self_critique.gen_cfg(**params)
+  outprefix = '%s.%d.%d.%s'%(
+    os.path.join(out_dir, '_'.join(ft_names)),
+    params['dim_hidden'], params['dim_input'],
+    model_spec)
+  model_cfg_file = '%s.model.json'%outprefix
+  model_cfg.save(model_cfg_file)
+
+  output_dir = outprefix
+  path_cfg = {
+    'trn_ftfiles': split_ftfiles[0],
+    'val_ftfiles': split_ftfiles[1],
+    'tst_ftfiles': split_ftfiles[2],
+    'split_dir': split_dir,
+    'annotation_dir': annotation_dir,
+    'output_dir': output_dir,
+    'model_file': os.path.join(output_dir, 'model', 'pretrain'),
+  }
+  path_cfg_file = '%s.path.json'%outprefix
+
+  if not os.path.exists(path_cfg['output_dir']):
+    os.mkdir(path_cfg['output_dir'])
+
+  json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
+
+
 if __name__ == '__main__':
-  prepare_vevd()
+  # prepare_vevd()
+  prepare_self_critique()
