980166c4a38211dfa427787f08990533d8e3b704
Jia Chen <jiac@t470>
2018-06-30 16:45:59 -0400

    sync

--- expr/tst.py
+++ expr/tst.py
@@ -14,7 +14,8 @@ def client():
         + "{\"hyp\": \"a white bird some sand water rocks and grass\",\"ref\":[\"a white bird sitting on top of a lake\",\"a bird cleans itself next to a body of water\",\"a large good standing in the dirt near the ocean\",\"a swan floating along the water nibbling at itself\"], \"id\": \"2\"}]";
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.connect(('127.0.0.1', 9090))
+  # sock.connect(('127.0.0.1', 9090))
+  sock.connect(('172.17.0.1', 9090))
 
   sock.sendall(tst_str + '\n') 
   f = sock.makefile()
