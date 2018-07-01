a512d5c5732524b656947e8258977ebe08133033
Jia Chen <jiac@t470>
2018-06-30 23:44:26 -0400

    sync

--- expr/prepare_gen_cfg.py
+++ expr/prepare_gen_cfg.py
@@ -8,6 +8,7 @@ import numpy as np
 import gen_model.vevd
 import gen_model.self_critique
 import gen_model.diversity
+import gen_model.margin
 
 
 '''func
@@ -203,7 +204,68 @@ def prepare_diversity():
   json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
 
 
+def prepare_margin():
+  root_dir = '/mnt/data1/jiac/trecvid2018/generation' # neptune
+  annotation_dir = os.path.join(root_dir, 'annotation')
+  split_dir = os.path.join(root_dir, 'split')
+  splits = ['trn', 'val', 'tst']
+  out_dir = os.path.join(root_dir, 'margin_expr')
+
+  ft_names = [
+    'i3d',
+    'resnet200',
+  ]
+
+  dim_fts, split_ftfiles = get_mean_ft_files(root_dir, ft_names, splits)
+
+  params = {
+    'num_step': 30,
+    'dim_input': 512,
+    'dim_hidden': 512,
+    'num_epoch': 100,
+    'content_keepin_prob': 1.,
+    'cell_keepin_prob': 0.5,
+    'cell_keepout_prob': 0.5,
+    'dim_fts': dim_fts,
+
+    'reward_alpha': .5,
+    'num_neg': 16,
+    'num_sample': 5,
+    'margin': .1,
+    'strategy': 'beam',
+    'metric': 'cider',
+  }
+
+  model_cfg = gen_model.margin.gen_cfg(**params)
+  outprefix = '%s.%d.%d.%.1f.%d.%d.%.1f.%s'%(
+    os.path.join(outdir, '_'.join(modal_feature_names)),
+    params['dim_hidden'], params['dim_input'],
+    params['reward_alpha'],
+    params['num_neg'], params['num_sample'], params['margin'],
+    params['metric'])
+  model_cfg_file = '%s.model.json'%outprefix
+  model_cfg.save(model_cfg_file)
+
+  output_dir = outprefix
+  path_cfg = {
+    'trn_ftfiles': split_ftfiles[0],
+    'val_ftfiles': split_ftfiles[1],
+    'tst_ftfiles': split_ftfiles[2],
+    'split_dir': split_dir,
+    'annotation_dir': annotation_dir,
+    'output_dir': output_dir,
+    'model_file': os.path.join(output_dir, 'model', 'pretrain'),
+  }
+  path_cfg_file = '%s.path.json'%outprefix
+
+  if not os.path.exists(path_cfg['output_dir']):
+    os.mkdir(path_cfg['output_dir'])
+
+  json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
+
+
 if __name__ == '__main__':
   # prepare_vevd()
   # prepare_self_critique()
-  prepare_diversity()
+  # prepare_diversity()
+  prepare_margin()
