166f11d696187b88abc3710faae6b437cfd8bbde
Jia Chen <jiac@t470>
2018-07-02 13:55:17 -0400

    sync

--- vision_expr/obj_detect.py
+++ vision_expr/obj_detect.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
-from object_detection.utils import vis_util
+from object_detection.utils import visualization_utils as vis_util
 
 
 '''func
