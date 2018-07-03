e402c51999647e3cfba5f73498884349ff2ef5cf
Jia Chen <jiac@t470>
2018-07-03 16:22:48 -0400

    sync

--- vision_expr/obj_detect.py
+++ vision_expr/obj_detect.py
@@ -1,14 +1,14 @@
 import os
 import subprocess
 
-# import tensorflow as tf
+import tensorflow as tf
 import numpy as np
 import imageio
 
-# from object_detection.utils import ops as utils_ops
-# from object_detection.utils import label_map_util
-# from object_detection.utils import visualization_utils as vis_util
-# from object_detection.utils import dataset_util
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import dataset_util
 
 
 '''func
