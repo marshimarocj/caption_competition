51a2f725160e3d93a4e50006fbfd6c258d2c3c96
Jia Chen <jiac@t470>
2018-07-02 20:54:22 -0400

    sync

--- vision_expr/obj_detect.py
+++ vision_expr/obj_detect.py
@@ -9,6 +9,7 @@ from PIL import GifImagePlugin
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import dataset_util
 
 
 '''func
