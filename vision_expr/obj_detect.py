67b774f44ccc458866de03729843da87aeb63659
Jia Chen <jiac@t470>
2018-07-03 12:58:42 -0400

    sync

--- vision_expr/obj_detect.py
+++ vision_expr/obj_detect.py
@@ -66,6 +66,63 @@ def run_inference_for_images(image_tensor, tensor_dict, images, sess):
   return output_dict
 
 
+def non_max_suppression_fast(boxes, overlapThresh):
+  # if there are no boxes, return an empty list
+  if len(boxes) == 0:
+    return []
+ 
+  # if the bounding boxes integers, convert them to floats --
+  # this is important since we'll be doing a bunch of divisions
+  if boxes.dtype.kind == "i":
+    boxes = boxes.astype("float")
+ 
+  # initialize the list of picked indexes 
+  pick = []
+ 
+  # grab the coordinates of the bounding boxes
+  x1 = boxes[:,0]
+  y1 = boxes[:,1]
+  x2 = boxes[:,2]
+  y2 = boxes[:,3]
+ 
+  # compute the area of the bounding boxes and sort the bounding
+  # boxes by the bottom-right y-coordinate of the bounding box
+  area = (x2 - x1 + 1) * (y2 - y1 + 1)
+  idxs = np.argsort(y2)
+ 
+  # keep looping while some indexes still remain in the indexes
+  # list
+  while len(idxs) > 0:
+    # grab the last index in the indexes list and add the
+    # index value to the list of picked indexes
+    last = len(idxs) - 1
+    i = idxs[last]
+    pick.append(i)
+ 
+    # find the largest (x, y) coordinates for the start of
+    # the bounding box and the smallest (x, y) coordinates
+    # for the end of the bounding box
+    xx1 = np.maximum(x1[i], x1[idxs[:last]])
+    yy1 = np.maximum(y1[i], y1[idxs[:last]])
+    xx2 = np.minimum(x2[i], x2[idxs[:last]])
+    yy2 = np.minimum(y2[i], y2[idxs[:last]])
+ 
+    # compute the width and height of the bounding box
+    w = np.maximum(0, xx2 - xx1 + 1)
+    h = np.maximum(0, yy2 - yy1 + 1)
+ 
+    # compute the ratio of overlap
+    overlap = (w * h) / area[idxs[:last]]
+ 
+    # delete all indexes from the index list that have
+    idxs = np.delete(idxs, np.concatenate(([last],
+      np.where(overlap > overlapThresh)[0])))
+ 
+  # return only the bounding boxes that were picked using the
+  # integer data type
+  return boxes[pick].astype("int")
+
+
 '''expr
 '''
 def tst():
@@ -274,9 +331,9 @@ def prepare_for_matlab():
   for name in names:
     detect_file = os.path.join(obj_detect_dir, name + '.npz')
     data = np.load(detect_file)
-    boxes = data['boxes']
-    classes = data['classes']
-    scores = data['score']
+    frame_boxes = data['boxes']
+    frame_classes = data['classes']
+    frame_scores = data['score']
 
     img_file = os.path.join(root_dir, 'imgs', name, '00000.jpg')
     img = Image.open(img_file)
@@ -288,20 +345,19 @@ def prepare_for_matlab():
     for f in range(0, num_frame, 16):
       out_file = os.path.join(obj_detect_dir, name + '.%d.box'%f)
       sort_idxs = np.argsort(-scores[f])
+      valid_idxs = sort_idxs[sort_idxs >= score_threshold]
+
+      boxes = frame_boxes[f][valid_idxs]
+      boxes[:, 0] *= img_h
+      boxes[:, 1] *= img_w
+      boxes[:, 2] *= img_h
+      boxes[:, 3] *= img_w
+
+      boxes = non_max_suppression_fast(boxes, 0.5)
+
       with open(out_file, 'w') as fout:
-        for idx in sort_idxs:
-          score = scores[f][idx]
-          if score < score_threshold:
-            break
-          c = classes[f][idx]
-          box = boxes[f][idx]
-          xmin = int(round(box[1]*img_w))
-          ymin = int(round(box[0]*img_h))
-          w = int(round((box[3] - box[1])*img_w))
-          h = int(round((box[2] - box[0])*img_h))
-          if w < min_size or h < min_size:
-            continue
-          fout.write('%d %d %d %d %d\n'%(xmin, ymin, w, h, c))
+        for box in boxes:
+          fout.write('%d %d %d %d\n'%(box[1], box[0], box[3]-box[1], box[2]-box[0]))
 
 
 if __name__ == '__main__':
