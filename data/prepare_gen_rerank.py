17e403359ce9a1d0d8a65cb7d37b5799e9bf0666
Jia Chen <jiac@t470>
2018-07-06 22:29:02 -0400

    sync

---
+++ data/prepare_gen_rerank.py
@@ -0,0 +1,20 @@
+import os
+import cPickle
+
+import numpy as np
+
+from prepare import caption2id_mask
+
+
+'''func
+'''
+
+
+'''expr
+'''
+def gen_captionid_mask():
+  root_dir = '/mnt/data1/jiac/trecvid2018/generation' # neptune
+
+
+if __name__ == '__main__':
+  pass
