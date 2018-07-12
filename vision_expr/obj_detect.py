85bdf7cb433595bd18d69756dba0c04af4541abe
Jia Chen <jiac@t470>
2018-07-12 12:43:18 -0400

    sync

--- vision_expr/obj_detect.py
+++ vision_expr/obj_detect.py
@@ -512,8 +512,8 @@ def prepare_for_track():
 
 
 def bat_prepare_for_track():
-  root_dir = '/home/jiac/data2/tgif/TGIF-Release/data' # gpu9
-  # root_dir = '/home/jiac/data/tgif' # gpu8
+  # root_dir = '/home/jiac/data2/tgif/TGIF-Release/data' # gpu9
+  root_dir = '/home/jiac/data/tgif' # gpu8
   lst_file = os.path.join(root_dir, 'tgif-v1.0.tsv')
   gif_dir = os.path.join(root_dir, 'gif')
   detect_dir = os.path.join(root_dir, 'obj_detect')
