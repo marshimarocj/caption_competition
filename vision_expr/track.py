710379f6caa9b6defe7e12fef2a01443e595d8fd
Jia Chen <jiac@t470>
2018-07-17 19:18:49 -0400

    sync

--- vision_expr/track.py
+++ vision_expr/track.py
@@ -786,8 +786,10 @@ def build_association_graph():
   # track_root_dir = os.path.join(root_dir, 'kcf_track')
 
   root_dir = '/mnt/data2/jiac/vtt_raw' # neptune
-  lst_file = os.path.join(root_dir, '16.lst')
-  track_root_dir = os.path.join(root_dir, '16_kcf_track')
+  # lst_file = os.path.join(root_dir, '16.lst')
+  # track_root_dir = os.path.join(root_dir, '16_kcf_track')
+  lst_file = os.path.join(root_dir, '17.lst')
+  track_root_dir = os.path.join(root_dir, '17_kcf_track')
 
   gap = 8
   iou_threshold = 0.5
