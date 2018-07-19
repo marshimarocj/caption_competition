67e24fa16841d6c54bbde7f2191538c087318034
Jia Chen <jiac@t470>
2018-07-19 13:31:24 -0400

    sync

--- expr/export_model.py
+++ expr/export_model.py
@@ -11,7 +11,7 @@ import framework.util.graph_ckpt
 '''
 def export_rank_video_embed():
   root_dir = '/data1/jiac/trecvid2018' # uranus
-  model_file = os.path.join(root_dir, 'rank', 'rnnve', 'i3d_resnet200.300.150.gru.max.0.5', 'model', 'epoch-77')
+  model_file = os.path.join(root_dir, 'rank', 'rnnve_expr', 'i3d_resnet200.300.150.gru.max.0.5', 'model', 'epoch-77')
 
   name2var = framework.util.graph_ckpt.load_variable_in_ckpt(model_file)
   print name2var.keys()
