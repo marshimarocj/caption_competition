a6c919a6768cfe44cacc8d1817988dedc09b4074
Jia Chen <jiac@t470>
2018-07-19 13:36:18 -0400

    sync

--- expr/export_model.py
+++ expr/export_model.py
@@ -14,7 +14,11 @@ def export_rank_video_embed():
   model_file = os.path.join(root_dir, 'rank', 'rnnve_expr', 'i3d_resnet200.300.150.gru.max.0.5', 'model', 'epoch-77')
 
   name2var = framework.util.graph_ckpt.load_variable_in_ckpt(model_file)
-  print name2var.keys()
+  # print name2var.keys()
+  ft_pca_W = name2var['rnnve.Model/ft_pca_W']
+  ft_pca_B = name2var['rnnve.Model/ft_pca_B']
+  print ft_pca_B
+  print type(ft_pca_B)
 
 
 if __name__ == '__main__':
