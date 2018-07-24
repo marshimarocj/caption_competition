7ba738e6c236a648fe51fde471a3e25c3eac7115
Jia Chen <jiac@t470>
2018-07-24 10:25:37 -0400

    sync

--- expr/prepare_rank_cfg.py
+++ expr/prepare_rank_cfg.py
@@ -9,6 +9,7 @@ import rank_model.ceve_score
 import rank_model.vevd_score
 import rank_model.aca
 import rank_model.aca_rnn
+import rank_model.aca_track
 
 
 '''func
@@ -391,10 +392,73 @@ def prepare_aca_rnn():
     json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
 
 
+def prepare_aca_track():
+  root_dir = '/mnt/data1/jiac/trecvid2018/rank' # neptune
+  split_dir = os.path.join(root_dir, 'split')
+  label_dir = os.path.join(root_dir, 'label')
+  word_file = os.path.join(root_dir, 'annotation', 'int2word.pkl')
+  embed_file = os.path.join(root_dir, 'annotation', 'E.word2vec.npy') 
+  out_dir = os.path.join(root_dir, 'aca_rnn_expr')
+  splits = ['trn', 'val', 'tst']
+
+  ft_names = [
+    'i3d',
+    'resnet200',
+  ]
+
+  params = {
+    'num_epoch': 50,
+
+    'margin': 0.1,
+    'alpha': 0.5,
+    'num_neg': 32,
+    'dim_ft': 1024 + 2048,
+    'dim_joint_embed': 300,
+    'num_track': 11,
+
+    'max_words_in_caption': 30,
+  }
+
+  outprefix = '%s.%d.%.1f'%(
+    os.path.join(out_dir, '_'.join(ft_names)), 
+    params['dim_joint_embed'], 
+    params['alpha'])
+
+  model_cfg = rank_model.aca_track.gen_cfg(**params)
+
+  model_cfg_file = '%s.model.json'%outprefix
+  model_cfg.save(model_cfg_file)
+
+  output_dir = outprefix
+  path_cfg = {
+    'trn_ftfiles': [os.path.join(root_dir, 'mp_feature', ft_name, 'trn_ft.npy') for ft_name in ft_names],
+    'val_ftfiles': [os.path.join(root_dir, 'mp_feature', ft_name, 'val_ft.2.npy') for ft_name in ft_names],
+    'tst_ftfiles': [],
+    'trn_att_ftfiles': [os.path.join(root_dir, 'sa_feature', ft_name, 'trn_ft.npy') for ft_name in ft_names],
+    'val_att_ftfiles': [os.path.join(root_dir, 'sa_feature', ft_name, 'val_ft.2.npy') for ft_name in ft_names],
+    'tst_att_ftfiles': [],
+    'val_label_file': os.path.join(label_dir, '17.set.2.gt'),
+    'trn_annotation_file': os.path.join(split_dir, 'trn_id_caption_mask.pkl'),
+    'val_annotation_file': os.path.join(split_dir, 'val_id_caption_mask.A.pkl'),
+    'tst_annotation_file': '',
+    'word_file': word_file,
+    'embed_file': embed_file,
+    'output_dir': output_dir,
+  }
+  path_cfg_file = '%s.path.json'%outprefix
+
+  if not os.path.exists(path_cfg['output_dir']):
+    os.mkdir(path_cfg['output_dir'])
+
+  with open(path_cfg_file, 'w') as fout:
+    json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
+
+
 if __name__ == '__main__':
   # prepare_ceve()
   # prepare_rnnve()
   # prepare_ceve_score()
   # prepare_vevd_score()
   # prepare_aca()
-  prepare_aca_rnn()
+  # prepare_aca_rnn()
+  prepare_aca_track()
