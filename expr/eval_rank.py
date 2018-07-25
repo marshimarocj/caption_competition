72a8dfd956d02560b1089df0e78fb377b52deace
jiac <jiac@thunderobot>
2018-07-25 12:02:44 -0400

    sync

--- expr/eval_rank.py
+++ expr/eval_rank.py
@@ -85,6 +85,7 @@ def predict_eval_trecvid17_B():
   root_dir = '/mnt/data1/jiac/trecvid2018/rank' # neptune
   ft_names = ['i3d', 'resnet200']
   ft_files = [os.path.join(root_dir, 'mp_feature', ft_name, 'val_ft.2.npy') for ft_name in ft_names]
+  track_ft_files = [os.path.join(root_dir, 'sa_feature', ft_name, 'val_ft.2.npz') for ft_name in ft_names]
   annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.B.pkl')
   out_name = 'val.B'
   # annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.A.pkl')
@@ -153,7 +154,8 @@ def predict_eval_trecvid17_B():
   best_epoch, mir_A = select_best_epoch(log_dir)
 
   p = gen_script_and_run(python_file, model_cfg_file, path_cfg_file, best_epoch, gpuid,
-    ft_files=','.join(ft_files), annotation_file=annotation_file, out_name=out_name)
+    # ft_files=','.join(ft_files), annotation_file=annotation_file, out_name=out_name)
+    ft_files=','.join(ft_files), att_ft_files=track_ft_files, annotation_file=annotation_file, out_name=out_name)
   p.wait()
 
   vid2gt = {}
