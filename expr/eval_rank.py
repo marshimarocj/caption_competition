6e8e3f41579e61c2baffd8b1760363bdf48584b8
Jia Chen <jiac@t470>
2018-07-26 13:48:41 -0400

    sync

--- expr/eval_rank.py
+++ expr/eval_rank.py
@@ -65,7 +65,7 @@ def report_best_epoch():
   # log_dir = os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.300.150.gru.max.0.5', 'log')
   # log_dir = os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.300.150.lstm.mean.0.5', 'log')
   # log_dir = os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.300.150.lstm.max.0.5', 'log')
-  log_dir = os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.512.256.gru.max.0.5.sbu', 'log')
+  # log_dir = os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.512.256.gru.max.0.5.sbu', 'log')
 
   # log_dir = os.path.join(root_dir, 'vevd_expr', 'i3d_resnet200.512.512.16.0.5.lstm', 'log')
 
@@ -73,6 +73,7 @@ def report_best_epoch():
   # log_dir = os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.300.0.5.att', 'log')
   # log_dir = os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.512.0.5.att.sbu', 'log')
   # log_dir = os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.300.0.5.att.feedforward', 'log')
+  log_dir = os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.300.0.5.att.flickr30m.feedforward', 'log')
   # log_dir = os.path.join(root_dir, 'aca_track_expr', 'i3d_resnet200.300.0.5', 'log')
 
   # log_dir = os.path.join(root_dir, 'aca_rnn_expr', 'i3d_resnet200.300.0.5', 'log')
@@ -220,6 +221,6 @@ def predict_eval_vevd():
 
 
 if __name__ == '__main__':
-  # report_best_epoch()
-  predict_eval_trecvid17_B()
+  report_best_epoch()
+  # predict_eval_trecvid17_B()
   # predict_eval_vevd()
