ba618a3df435e9883b5a2f5f1126e0c4752f2bf0
Jia Chen <jiac@t470>
2018-07-26 22:37:50 -0400

    sync

--- data/prepare.py
+++ data/prepare.py
@@ -51,33 +51,37 @@ def process_sent(sentence, allow_digits=True):
 '''expr
 '''
 def merge_tgif_trecvid16_rank_trn():
-  trecvid_root_dir = '/data1/jiac/trecvid2017/rank' # mercurial
-  tgif_root_dir = '/data1/jiac/tgif'
-  out_root_dir = '/data1/jiac/trecvid2018/rank'
+  # trecvid_root_dir = '/data1/jiac/trecvid2017/rank' # mercurial
+  # tgif_root_dir = '/data1/jiac/tgif'
+  # out_root_dir = '/data1/jiac/trecvid2018/rank'
+  trecvid_root_dir = '/mnt/data1/jiac/trecvid2017' # neptune
+  tgif_root_dir = '/mnt/data1/jiac/tgif'
+  out_root_dir = '/mnt/data1/jiac/trecvid2018/rank'
 
   ##########ft#########
-  # tgif_ft_files = [
-  #   'trn_ft.npy',
-  #   'val_ft.npy',
-  #   'tst_ft.npy',
-  # ]
-  # trecvid_ft_files = [
-  #   'tst_ft.npy',
-  # ]
+  tgif_ft_files = [
+    'trn_ft.npy',
+    'val_ft.npy',
+    'tst_ft.npy',
+  ]
+  trecvid_ft_files = [
+    'tst_ft.npy',
+  ]
   # for ft_name in ['i3d', 'resnet200']:
-  #   print ft_name
-  #   fts = []
-  #   for tgif_ft_file in tgif_ft_files:
-  #     file = os.path.join(tgif_root_dir, 'mp_feature', ft_name, tgif_ft_file)
-  #     ft = np.load(file)
-  #     fts.append(ft)
-  #   for trecivd_ft_file in trecvid_ft_files:
-  #     file = os.path.join(trecvid_root_dir, 'mp_feature', ft_name, trecivd_ft_file)
-  #     ft = np.load(file)
-  #     fts.append(ft)
-  #   fts = np.concatenate(fts, 0)
-  #   out_file = os.path.join(out_root_dir, 'mp_feature', ft_name, 'trn_ft.npy')
-  #   np.save(out_file, fts)
+  for ft_name in ['i3d_flow']:
+    print ft_name
+    fts = []
+    for tgif_ft_file in tgif_ft_files:
+      file = os.path.join(tgif_root_dir, 'mp_feature', ft_name, tgif_ft_file)
+      ft = np.load(file)
+      fts.append(ft)
+    for trecivd_ft_file in trecvid_ft_files:
+      file = os.path.join(trecvid_root_dir, 'mp_feature', ft_name, trecivd_ft_file)
+      ft = np.load(file)
+      fts.append(ft)
+    fts = np.concatenate(fts, 0)
+    out_file = os.path.join(out_root_dir, 'mp_feature', ft_name, 'trn_ft.npy')
+    np.save(out_file, fts)
 
   #########caption mask ##########
   # tgif_caption_mask_files = [
@@ -130,21 +134,21 @@ def merge_tgif_trecvid16_rank_trn():
   # np.save(out_file, vids)
 
   ##########caption_dict##########
-  tgif_caption_file = os.path.join(tgif_root_dir, 'annotation', 'human_caption_dict.pkl')
-  trecvid_caption_file = os.path.join(trecvid_root_dir, 'annotation', 'human_caption_dict.pkl')
-  vid2captions = {}
-  with open(tgif_caption_file) as f:
-    data = cPickle.load(f)
-  vid2captions.update(data)
-  base = len(vid2captions)
-  with open(trecvid_caption_file) as f:
-    data = cPickle.load(f)
-  start = len(data) - 1915
-  for vid in range(start, start + 1915):
-    vid2captions[vid - start + base] = data[vid]
-  out_file = os.path.join(out_root_dir, 'annotation', 'human_caption_dict.pkl')
-  with open(out_file, 'w') as fout:
-    cPickle.dump(vid2captions, fout)
+  # tgif_caption_file = os.path.join(tgif_root_dir, 'annotation', 'human_caption_dict.pkl')
+  # trecvid_caption_file = os.path.join(trecvid_root_dir, 'annotation', 'human_caption_dict.pkl')
+  # vid2captions = {}
+  # with open(tgif_caption_file) as f:
+  #   data = cPickle.load(f)
+  # vid2captions.update(data)
+  # base = len(vid2captions)
+  # with open(trecvid_caption_file) as f:
+  #   data = cPickle.load(f)
+  # start = len(data) - 1915
+  # for vid in range(start, start + 1915):
+  #   vid2captions[vid - start + base] = data[vid]
+  # out_file = os.path.join(out_root_dir, 'annotation', 'human_caption_dict.pkl')
+  # with open(out_file, 'w') as fout:
+  #   cPickle.dump(vid2captions, fout)
 
 
 def prepare_trecvid17_rank_val():
@@ -593,7 +597,7 @@ def prepare_trecvid17_flow_ft():
 
 
 if __name__ == '__main__':
-  # merge_tgif_trecvid16_rank_trn()
+  merge_tgif_trecvid16_rank_trn()
   # prepare_trecvid17_rank_val()
   # prepare_trecvid17_rank_gen_val()
 
@@ -605,4 +609,4 @@ if __name__ == '__main__':
 
   # prepare_tgif_flow_ft()
   # prepare_trecvid16_flow_ft()
-  prepare_trecvid17_flow_ft()
+  # prepare_trecvid17_flow_ft()
