a192bbe4e6ccadc73e58e1a2ff925aec37497ebb
Jia Chen <jiac@t470>
2018-07-28 15:04:28 -0400

    sync

--- expr/rerank.py
+++ expr/rerank.py
@@ -27,8 +27,8 @@ def graph_match_rerank():
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.flickr30m', 'pred', 'val.B.npy'),
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.1.0.flickr30m', 'pred', 'val.A.npy'),
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.1.0.flickr30m', 'pred', 'val.B.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.npy'),
+    # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.npy'),
+    # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.npy'),
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.max.0.5.score', 'pred', 'val.A.npy'),
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.max.0.5.score', 'pred', 'val.B.npy'),
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.300.150.lstm.max.0.5', 'pred', 'val.A.npy'),
@@ -43,6 +43,8 @@ def graph_match_rerank():
     # os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.300.0.5.att.feedforward', 'pred', 'val.B.npy'),
     # os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.500.0.5.att.flickr30m.feedforward', 'pred', 'val.A.npy'),
     # os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.500.0.5.att.flickr30m.feedforward', 'pred', 'val.B.npy'),
+    os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.500.0.5.0.1.att.flickr30m.feedforward', 'pred', 'val.A.npy'),
+    os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.500.0.5.0.1.att.flickr30m.feedforward', 'pred', 'val.B.npy'),
   ]
   out_files = [
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.max.1.0', 'pred', 'val.A.rerank.20.npy'),
@@ -55,8 +57,8 @@ def graph_match_rerank():
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.flickr30m', 'pred', 'val.B.rerank.20.npy'),
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.1.0.flickr30m', 'pred', 'val.A.rerank.20.npy'),
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.1.0.flickr30m', 'pred', 'val.B.rerank.20.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.rerank.20.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.rerank.20.npy'),
+    # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.rerank.20.npy'),
+    # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.rerank.20.npy'),
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.max.0.5.score', 'pred', 'val.A.rerank.20.npy'),
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.max.0.5.score', 'pred', 'val.B.rerank.20.npy'),
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.300.150.lstm.max.0.5', 'pred', 'val.A.rerank.20.npy'),
@@ -71,6 +73,8 @@ def graph_match_rerank():
     # os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.300.0.5.att.feedforward', 'pred', 'val.B.rerank.20.npy'),
     # os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.500.0.5.att.flickr30m.feedforward', 'pred', 'val.A.rerank.20.npy'),
     # os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.500.0.5.att.flickr30m.feedforward', 'pred', 'val.B.rerank.20.npy'),
+    os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.500.0.5.0.1.att.flickr30m.feedforward', 'pred', 'val.A.rerank.20.npy'),
+    os.path.join(root_dir, 'aca_expr', 'i3d_resnet200.500.0.5.0.1.att.flickr30m.feedforward', 'pred', 'val.B.rerank.20.npy'),
   ]
 
   topk = 20
@@ -190,5 +194,5 @@ def eval_rerank():
 
 
 if __name__ == '__main__':
-  # graph_match_rerank()
-  eval_rerank()
+  graph_match_rerank()
+  # eval_rerank()
