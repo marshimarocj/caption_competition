d087c02159967fd43cd865bdea74a4a6bfa957b8
Jia Chen <jiac@t470>
2018-07-28 21:05:51 -0400

    sync

--- expr/prepare_gen_cfg.py
+++ expr/prepare_gen_cfg.py
@@ -6,6 +6,7 @@ sys.path.append('../')
 import numpy as np
 
 import gen_model.vevd
+import gen_model.devd
 import gen_model.self_critique
 import gen_model.diversity
 import gen_model.margin
@@ -78,6 +79,60 @@ def prepare_vevd():
   }
 
   model_cfg = gen_model.vevd.gen_cfg(**params)
+  outprefix = '%s.%d.%d.%s'%(
+    os.path.join(out_dir, '_'.join(ft_names)),
+    params['dim_hidden'], params['dim_input'],
+    model_spec)
+  model_cfg_file = '%s.model.json'%outprefix
+  model_cfg.save(model_cfg_file)
+
+  output_dir = outprefix
+  path_cfg = {
+    'trn_ftfiles': split_ftfiles[0],
+    'val_ftfiles': split_ftfiles[1],
+    'tst_ftfiles': split_ftfiles[2],
+    'split_dir': split_dir,
+    'annotation_dir': annotation_dir,
+    'output_dir': output_dir,
+  }
+  path_cfg_file = '%s.path.json'%outprefix
+
+  if not os.path.exists(path_cfg['output_dir']):
+    os.mkdir(path_cfg['output_dir'])
+
+  json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
+
+
+def prepare_devd():
+  root_dir = '/mnt/data1/jiac/trecvid2018/generation' # neptune
+  annotation_dir = os.path.join(root_dir, 'annotation')
+  split_dir = os.path.join(root_dir, 'split')
+  splits = ['trn', 'val', 'tst']
+  out_dir = os.path.join(root_dir, 'devd_expr')
+  model_spec = 'lstm'
+
+  ft_names = [
+    'i3d',
+    'resnet200',
+    'i3d_flow',
+    # 'i3d_flow_max',
+  ]
+
+  dim_fts, split_ftfiles = get_mean_ft_files(root_dir, ft_names, splits)
+
+  params = {
+    'num_step': 30,
+    'dim_input': 512,
+    'dim_hidden': 512,
+    'num_epoch': 200,
+    'content_keepin_prob': 1.,
+    'cell_keepin_prob': 0.5,
+    'cell_keepout_prob': 0.5,
+    'dim_ft': dim_fts,
+    'dim_ft_hiddens': [1024],
+  }
+
+  model_cfg = gen_model.devd.gen_cfg(**params)
   outprefix = '%s.%d.%d.%s'%(
     os.path.join(out_dir, '_'.join(ft_names)),
     params['dim_hidden'], params['dim_input'],
@@ -342,7 +397,8 @@ def prepare_vead():
 
 if __name__ == '__main__':
   # prepare_vevd()
+  prepare_devd()
   # prepare_self_critique()
   # prepare_diversity()
   # prepare_margin()
-  prepare_vead()
+  # prepare_vead()
