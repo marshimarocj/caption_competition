3a9591fb4d4694836ff89e202f0d0c467711905c
Jia Chen <jiac@t470>
2018-07-30 19:56:45 -0400

    sync

--- expr/inverse_rl.py
+++ expr/inverse_rl.py
@@ -3,13 +3,13 @@ import cPickle
 import sys
 import random
 import json
+import socket
 sys.path.append('../')
 
 import numpy as np
 
 from bleu.bleu import Bleu
 from rouge.rouge import Rouge
-from meteor.meteor import Meteor
 from service.fast_cider import CiderScorer, cook_test
 
 
@@ -49,6 +49,9 @@ def calc_metric_fts():
     vid2captions = cPickle.load(f)
   cider_scorer.init_refs(vid2captions)
 
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.connect(('127.0.0.1', 10000))
+
   outs = []
   cnt = 0
   for tst_vid in tst_vids:
@@ -68,14 +71,24 @@ def calc_metric_fts():
         pred = {0:[captions[j]]}
 
         res_bleu, _ = bleu_scorer.compute_score(gt, pred)
-        res_meteor, _ = meteor_scorer.compute_score(gt, pred)
-        meteor_scorer.meteor_p.kill()
         res_rouge, _ = rouge_scorer.compute_score(gt, pred)
 
         pred_vec, pred_norm, pred_length = cider_scorer._counts2vec(cook_test(pred[0][0]))
         res_cider = cider_scorer._sim(pred_vec, gt_vec, pred_norm, gt_norm, pred_length, gt_length)        
         res_cider = np.mean(res_cider)
         res_cider *= 10.0
+
+        msg = json.dumps([{
+          'hyp': pred[0][0],
+          'ref': gt[0][0],
+          'id': 0,
+        }])
+        sock.sendall(msg.encode('utf8'))
+        f = sock.makefile()
+        line = f.readline()
+        line = line.strip()
+        id_scores = json.load(line)
+        res_meteor = id_scores[0]['score']
 
         outs.append({
           'pred_id': (tst_vid, j),
@@ -88,7 +101,6 @@ def calc_metric_fts():
         })
 
         bleu_scorer = Bleu(4)
-        meteor_scorer = Meteor()
         rouge_scorer = Rouge()
 
         idx =random.randint(0, len(trn_vids-1))
@@ -96,14 +108,24 @@ def calc_metric_fts():
         pred = {0: [vid2captions[trn_vid][0]]}
 
         res_bleu, _ = bleu_scorer.compute_score(gt, pred)
-        res_meteor, _ = meteor_scorer.compute_score(gt, pred)
-        meteor_scorer.meteor_p.kill()
         res_rouge, _ = rouge_scorer.compute_score(gt, pred)
 
         pred_vec, pred_norm, pred_length = cider_scorer._counts2vec(cook_test(pred[0][0]))
         res_cider = cider_scorer._sim(pred_vec, gt_vec, pred_norm, gt_norm, pred_length, gt_length)        
         res_cider = np.mean(res_cider)
         res_cider *= 10.0
+
+        msg = json.dumps([{
+          'hyp': pred[0][0],
+          'ref': gt[0][0],
+          'id': 0,
+        }])
+        sock.sendall(msg.encode('utf8'))
+        f = sock.makefile()
+        line = f.readline()
+        line = line.strip()
+        id_scores = json.load(line)
+        res_meteor = id_scores[0]['score']
 
         outs.append({
           'pred_id': (trn_vid, 0),
@@ -116,8 +138,9 @@ def calc_metric_fts():
         })
 
     cnt += 1
-    if cnt % 500 == 0:
+    if cnt % 50 == 0:
       print cnt
+  sock.close()
 
   with open(out_file, 'w') as fout:
     json.dump(outs, fout, indent=2)
