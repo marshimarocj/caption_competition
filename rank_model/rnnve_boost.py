9a6cf860094e74323ae0eabe731d75390f9d92ac
Jia Chen <jiac@t470>
2018-07-31 19:34:19 -0400

    sync

--- rank_model/rnnve_boost.py
+++ rank_model/rnnve_boost.py
@@ -219,7 +219,7 @@ class Model(rnnve_orth.Model):
           contrast_ft_loss = tf.maximum(contrast_ft_loss, tf.zeros_like(contrast_ft_loss))
           self.op2monitor['contrast_ft_loss_%d'%g] = tf.reduce_sum(contrast_ft_loss)
 
-          group_loss = self._config.alpha * contrast_caption_loss + (1. - alpha) * contrast_ft_loss
+          group_loss = self._config.alpha * contrast_caption_loss + (1. - self._config.alpha) * contrast_ft_loss
           loss += tf.reduce_sum(w * group_loss)
           self.op2monitor['loss_%d'] = tf.reduce_sum(group_loss)
 
