dad9d2ef9ea49a22bea13f62e9a11f5bf692670e
Jia Chen <jiac@t470>
2018-08-02 15:29:44 -0400

    sync

--- expr/prepare_rank_cfg.py
+++ expr/prepare_rank_cfg.py
@@ -14,7 +14,7 @@ import rank_model.aca_freeze
 import rank_model.rnnve_feedforward
 import rank_model.rnnve_orth
 import rank_model.rnnve_boost
-# import rank_model.align
+import rank_model.align
 
 
 '''func
@@ -747,6 +747,65 @@ def prepare_rnnve_boost():
     json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
 
 
+def prepare_align():
+  root_dir = '/home/jiac/data/trecvid2018/rank' # gpu8
+  split_dir = os.path.join(root_dir, 'split')
+  label_dir = os.path.join(root_dir, 'label')
+  word_file = os.path.join(root_dir, 'annotation', 'int2word.pkl')
+  embed_file = os.path.join(root_dir, 'annotation', 'E.flickr30m.word2vec.npy') 
+  out_dir = os.path.join(root_dir, 'align_expr')
+  splits = ['trn', 'val', 'tst']
+
+  ft_names = [
+    'i3d',
+    'resnet200',
+  ]
+
+  params = {
+    'num_epoch': 50,
+
+    'alpha': 0.5,
+    'num_neg': 32,
+    'dim_ft': 1024 + 2048,
+    'dim_joint_embed': 500,
+    'margin': .1,
+
+    'max_words_in_caption': 30,
+    'lr_mult': .1,
+  }
+
+  outprefix = '%s.%d.%.1f.%.1f.flickr30m'%(
+    os.path.join(out_dir, '_'.join(ft_names)), 
+    params['dim_joint_embed'], 
+    params['alpha'], params['lr_mult'])
+
+  model_cfg = rank_model.align.gen_cfg(**params)
+
+  model_cfg_file = '%s.model.json'%outprefix
+  model_cfg.save(model_cfg_file)
+
+  output_dir = outprefix
+  path_cfg = {
+    'trn_ftfiles': [os.path.join(root_dir, 'mp_feature', ft_name, 'trn_ft.npy') for ft_name in ft_names],
+    'val_ftfiles': [os.path.join(root_dir, 'mp_feature', ft_name, 'val_ft.2.npy') for ft_name in ft_names],
+    'tst_ftfiles': [],
+    'val_label_file': os.path.join(label_dir, '17.set.2.gt'),
+    'trn_annotation_file': os.path.join(split_dir, 'trn_id_caption_mask.pkl'),
+    'val_annotation_file': os.path.join(split_dir, 'val_id_caption_mask.A.pkl'),
+    'tst_annotation_file': '',
+    'word_file': word_file,
+    'embed_file': embed_file,
+    'output_dir': output_dir,
+  }
+  path_cfg_file = '%s.path.json'%outprefix
+
+  if not os.path.exists(path_cfg['output_dir']):
+    os.mkdir(path_cfg['output_dir'])
+
+  with open(path_cfg_file, 'w') as fout:
+    json.dump(path_cfg, open(path_cfg_file, 'w'), indent=2)
+
+
 if __name__ == '__main__':
   # prepare_ceve()
   # prepare_rnnve()
@@ -755,7 +814,8 @@ if __name__ == '__main__':
   # prepare_aca()
   # prepare_aca_rnn()
   # prepare_aca_track()
-  prepare_aca_freeze()
+  # prepare_aca_freeze()
   # prepare_rnnve_feedforward()
   # prepare_rnnve_orth()
   # prepare_rnnve_boost()
+  prepare_align()
