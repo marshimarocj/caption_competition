7796ff5c1012f35d15fcba4c723926d2df4d9bc8
Jia Chen <jiac@t470>
2018-08-03 23:31:20 -0400

    sync

--- expr/eval_rank.py
+++ expr/eval_rank.py
@@ -308,18 +308,19 @@ def get_embeds():
   out_names = ['val.%s'%alpha for alpha in ['A', 'B']]
 
   # expr_name = os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.flickr30m')
-  # log_dir = os.path.join(expr_name, 'log')
-  # model_cfg_file = '%s.model.json'%expr_name
-  # path_cfg_file = '%s.path.json'%expr_name
-  # python_file = '../rank_driver/rnnve_embed.py'
-  # gpuid = 3
-
-  expr_name = os.path.join(root_dir, 'rnnve_orth_expr', 'i3d_resnet200.512_512_512.250.gru.max.0.5.0.1.flickr30m.freeze.direct')
+  expr_name = os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m')
   log_dir = os.path.join(expr_name, 'log')
   model_cfg_file = '%s.model.json'%expr_name
   path_cfg_file = '%s.path.json'%expr_name
-  python_file = '../rank_driver/rnnve_ensemble_embed.py'
-  gpuid = 1
+  python_file = '../rank_driver/rnnve_embed.py'
+  gpuid = 0
+
+  # expr_name = os.path.join(root_dir, 'rnnve_orth_expr', 'i3d_resnet200.512_512_512.250.gru.max.0.5.0.1.flickr30m.freeze.direct')
+  # log_dir = os.path.join(expr_name, 'log')
+  # model_cfg_file = '%s.model.json'%expr_name
+  # path_cfg_file = '%s.path.json'%expr_name
+  # python_file = '../rank_driver/rnnve_ensemble_embed.py'
+  # gpuid = 1
 
   best_epoch, _ = select_best_epoch(log_dir)
 
@@ -354,7 +355,7 @@ def get_rnn_output():
 
 if __name__ == '__main__':
   # report_best_epoch()
-  predict_eval_trecvid17_B()
+  # predict_eval_trecvid17_B()
   # predict_eval_vevd()
-  # get_embeds()
+  get_embeds()
   # get_rnn_output()
