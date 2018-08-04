c118565b71cee24f0a9552d95ac1f32f9b9164ce
Jia Chen <jiac@t470>
2018-08-03 23:29:25 -0400

    sync

--- expr/rerank.py
+++ expr/rerank.py
@@ -186,8 +186,8 @@ def eval_rerank():
     # os.path.join(root_dir, 'rnnve_orth_expr', 'i3d_resnet200.512_512_512.250.gru.max.0.5.0.1.flickr30m.freeze.direct', 'pred', 'val.A.rerank.20.npy'),
     # os.path.join(root_dir, 'rnnve_orth_expr', 'i3d_resnet200.512_512_512.250.gru.max.0.5.0.1.flickr30m.freeze', 'pred', 'val.B.npy'),
     # os.path.join(root_dir, 'rnnve_orth_expr', 'i3d_resnet200.512_512_512.250.gru.max.0.5.0.1.flickr30m.freeze', 'pred', 'val.B.rerank.20.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.rerank.20.npy'),
+    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.npy'),
+    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.rerank.20.npy'),
   ]
   label_file = os.path.join(root_dir, 'label', '17.set.2.gt')
 
@@ -197,7 +197,7 @@ def eval_rerank():
       line = line.strip()
       data = line.split(' ')
       vid = int(data[0])
-      gt = int(data[2])
+      gt = int(data[1])
       vid2gt[vid] = gt
 
   predicts = np.load(pred_files[0])
