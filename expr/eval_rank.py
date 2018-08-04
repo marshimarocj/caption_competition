b4b8f3a82732339a939ae187f0502e0f6ba06db0
Jia Chen <jiac@t470>
2018-08-04 11:56:39 -0400

    sync

--- expr/eval_rank.py
+++ expr/eval_rank.py
@@ -114,7 +114,7 @@ def predict_eval_trecvid17_B():
   # root_dir = '/mnt/data1/jiac/trecvid2018/rank' # neptune
   root_dir = '/home/jiac/data/trecvid2018/rank' # gpu8
   # root_dir = '/home/jiac/data/trecvid2018/rank' # gpu9
-  ft_names = ['i3d', 'i3d_flow', 'resnet200']
+  ft_names = ['i3d', 'resnet200']
   ft_files = [os.path.join(root_dir, 'mp_feature', ft_name, 'val_ft.2.npy') for ft_name in ft_names]
   track_ft_files = [os.path.join(root_dir, 'sa_feature', ft_name, 'val_ft.2.npz') for ft_name in ft_names]
   # annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.B.pkl')
