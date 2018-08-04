f6e175d80cc68d4aecd3c5615d0ad1bb2eaee872
Jia Chen <jiac@t470>
2018-08-03 23:50:12 -0400

    sync

--- expr/rerank.py
+++ expr/rerank.py
@@ -49,8 +49,9 @@ def graph_match_rerank():
     # os.path.join(root_dir, 'rnnve_orth_expr', 'i3d_resnet200.512_512_512.250.gru.max.0.5.0.1.flickr30m.freeze.direct', 'pred', 'val.B.npy'),
     # os.path.join(root_dir, 'rnnve_orth_expr', 'i3d_resnet200.512_512_512.250.gru.max.0.5.0.1.flickr30m.freeze', 'pred', 'val.A.npy'),
     # os.path.join(root_dir, 'rnnve_orth_expr', 'i3d_resnet200.512_512_512.250.gru.max.0.5.0.1.flickr30m.freeze', 'pred', 'val.B.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.npy'),
+    # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.npy'),
+    # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.npy'),
+    os.path.join(root_dir, 'rnnve_expr', 'i3d_i3d_flow_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.npy'),
   ]
   out_files = [
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.max.1.0', 'pred', 'val.A.rerank.20.npy'),
@@ -327,8 +328,8 @@ def corr():
 
 
 if __name__ == '__main__':
-  # graph_match_rerank()
+  graph_match_rerank()
   # eval_rerank()
   # gen_caption_sim_mat()
-  rwr()
+  # rwr()
   # corr()
