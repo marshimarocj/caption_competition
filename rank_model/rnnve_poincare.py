ff83093233a458b4f16a0539c5ce2aa2fec10194
Jia Chen <jiac@t470>
2018-08-04 22:15:15 -0400

    sync

--- rank_model/rnnve_poincare.py
+++ rank_model/rnnve_poincare.py
@@ -177,7 +177,9 @@ class Model(framework.model.module.AbstractModel):
       self.op2monitor['caption_embed_norm'] = tf.reduce_mean(tf.norm(caption_embed, axis=-1))
       caption_embed = framework.util.expanded_op.poincareball_gradient(caption_embed)
 
-      ft_embed = tf.nn.xw_plus_b(in_ops[self.InKey.FT], self.ft_pca_W, self.ft_pca_B)
+      fts = in_ops[self.InKey.FT]
+      fts = tf.layers.batch_normalization(fts, training=in_ops[self.InKey.IS_TRN])
+      ft_embed = tf.nn.xw_plus_b(fts, self.ft_pca_W, self.ft_pca_B)
       ft_embed = tf.nn.tanh(ft_embed)
       # unit ball
       ft_embed /= self._config.dim_joint_embed**0.5
@@ -252,11 +254,11 @@ class Model(framework.model.module.AbstractModel):
 
       contrast_caption_loss = neg_caption_sim + self._config.margin - pos_sim
       contrast_caption_loss = tf.maximum(contrast_caption_loss, tf.zeros_like(contrast_caption_loss))
-      self.op2monitor['contrast_caption_loss'] = tf.reduce_mean(contrast_caption_loss)
+      # self.op2monitor['contrast_caption_loss'] = tf.reduce_mean(contrast_caption_loss)
 
       contrast_ft_loss = neg_ft_sim + self._config.margin - pos_sim
       contrast_ft_loss = tf.maximum(contrast_ft_loss, tf.zeros_like(contrast_ft_loss))
-      self.op2monitor['contrast_ft_loss'] = tf.reduce_mean(contrast_ft_loss)
+      # self.op2monitor['contrast_ft_loss'] = tf.reduce_mean(contrast_ft_loss)
 
       loss = self._config.alpha * contrast_caption_loss + (1.0 - self._config.alpha) * contrast_ft_loss
       loss = tf.reduce_mean(loss)
