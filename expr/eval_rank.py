2fefab1fc3e40acf1a0b71733318fd6d50b254f2
Jia Chen <jiac@t470>
2018-08-06 13:14:05 -0400

    sync

--- expr/eval_rank.py
+++ expr/eval_rank.py
@@ -89,7 +89,9 @@ def report_best_epoch():
 
   # log_dir = os.path.join(root_dir, 'rnnve_mm_expr', 'i3d_i3d_flow_resnet200.512_512.max.0.5.flickr30m.freeze', 'log')
 
-  log_dir = os.path.join(root_dir, 'rnnve_adv_expr', 'i3d_resnet200.512_512_512.max.0.5.0.1.flickr30m.freeze', 'log')
+  # log_dir = os.path.join(root_dir, 'rnnve_adv_expr', 'i3d_resnet200.512_512_512.max.0.5.0.1.flickr30m.freeze', 'log')
+
+  log_dir = os.path.join(root_dir, 'rnn_attwv_expr', 'i3d_resnet200.512.250.gru.max.0.5.0.1.flickr30m', 'log')
 
   # log_dir = os.path.join(root_dir, 'vevd_expr', 'i3d_resnet200.512.512.16.0.5.lstm', 'log')
 
@@ -372,8 +374,8 @@ def get_rnn_output():
 
 
 if __name__ == '__main__':
-  # report_best_epoch()
-  predict_eval_trecvid17_B()
+  report_best_epoch()
+  # predict_eval_trecvid17_B()
   # predict_eval_vevd()
   # get_embeds()
   # get_rnn_output()
