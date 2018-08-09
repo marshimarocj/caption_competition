09dadf42f28b534016385045064703977811e94c
Jia Chen <jiac@t470>
2018-08-08 22:59:55 -0400

    sync

--- rank_model/rnnve.py
+++ rank_model/rnnve.py
@@ -233,19 +233,24 @@ class Model(framework.model.module.AbstractModel):
       if self._config.loss == 'lifted':
         contrast_caption_loss = neg_caption_sim + self._config.margin - pos_sim
         contrast_caption_loss = tf.maximum(contrast_caption_loss, tf.zeros_like(contrast_caption_loss))
+        self.op2monitor['contrast_caption_loss'] = tf.reduce_sum(contrast_caption_loss)
       else:
         contrast_caption_loss = neg_caption_sim - pos_sim
-      self.op2monitor['contrast_caption_loss'] = tf.reduce_sum(contrast_caption_loss)
+        self.op2monitor['contrast_caption_loss'] = tf.reduce_mean(contrast_caption_loss)
 
       if self._config.loss == 'lifted':
         contrast_ft_loss = neg_ft_sim + self._config.margin - pos_sim
         contrast_ft_loss = tf.maximum(contrast_ft_loss, tf.zeros_like(contrast_ft_loss))
+        self.op2monitor['contrast_ft_loss'] = tf.reduce_sum(contrast_ft_loss)
       else:
         contrast_ft_loss = neg_ft_sim - pos_sim
-      self.op2monitor['contrast_ft_loss'] = tf.reduce_sum(contrast_ft_loss)
+        self.op2monitor['contrast_ft_loss'] = tf.reduce_mean(contrast_caption_loss)
 
       loss = self._config.alpha * contrast_caption_loss + (1.0 - self._config.alpha) * contrast_ft_loss
-      loss = tf.reduce_sum(loss)
+      if self._config.loss == 'lifted':
+        loss = tf.reduce_sum(loss)
+      else:
+        loss = tf.reduce_mean(loss)
       self.op2monitor['loss'] = loss
     return loss
 
