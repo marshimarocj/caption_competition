296bd85c5008b0138d147893b4ad38cdf100431a
Jia Chen <jiac@t470>
2018-08-10 15:12:53 -0400

    sync

--- expr/rerank.py
+++ expr/rerank.py
@@ -159,8 +159,8 @@ def graph_match_rerank():
 
 
 def eval_rerank():
-  # root_dir = '/data1/jiac/trecvid2018/rank' # uranus
-  root_dir = '/mnt/data1/jiac/trecvid2018/rank' # neptune
+  root_dir = '/data1/jiac/trecvid2018/rank' # uranus
+  # root_dir = '/mnt/data1/jiac/trecvid2018/rank' # neptune
   pred_files = [
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.max.1.0', 'pred', 'val.B.npy'),
     # os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.max.1.0', 'pred', 'val.B.rerank.20.npy'),
@@ -196,8 +196,10 @@ def eval_rerank():
     # os.path.join(root_dir, 'rnnve_orth_expr', 'i3d_resnet200.512_512_512.250.gru.max.0.5.0.1.flickr30m.freeze', 'pred', 'val.B.rerank.20.npy'),
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.npy'),
     # os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.1000.500.gru.max.0.5.0.1.flickr30m', 'pred', 'val.A.rerank.20.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_i3d_flow_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.npy'),
-    os.path.join(root_dir, 'rnnve_expr', 'i3d_i3d_flow_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.rerank.20.npy'),
+    # os.path.join(root_dir, 'rnnve_expr', 'i3d_i3d_flow_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.npy'),
+    # os.path.join(root_dir, 'rnnve_expr', 'i3d_i3d_flow_resnet200.500.250.gru.max.0.5.0.1.flickr30m', 'pred', 'val.B.rerank.20.npy'),
+    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.lifted.0.3.flickr30m', 'pred', 'val.B.npy'),
+    os.path.join(root_dir, 'rnnve_expr', 'i3d_resnet200.500.250.gru.max.0.5.0.1.lifted.0.3.flickr30m', 'pred', 'val.B.rerank.20.npy'),
   ]
   label_file = os.path.join(root_dir, 'label', '17.set.2.gt')
 
@@ -337,8 +339,8 @@ def corr():
 
 
 if __name__ == '__main__':
-  graph_match_rerank()
-  # eval_rerank()
+  # graph_match_rerank()
+  eval_rerank()
   # gen_caption_sim_mat()
   # rwr()
   # corr()
