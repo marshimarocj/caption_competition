567f52092800e62669298edb29c0e5ed3fee5d2d
Jia Chen <jiac@t470>
2018-08-11 13:45:18 -0400

    sync

--- expr/eval_rank.py
+++ expr/eval_rank.py
@@ -164,11 +164,11 @@ def predict_eval_trecvid17_B():
   ft_names = ['i3d', 'i3d_flow', 'resnet200']
   ft_files = [os.path.join(root_dir, 'mp_feature', ft_name, 'val_ft.2.npy') for ft_name in ft_names]
   track_ft_files = [os.path.join(root_dir, 'sa_feature', ft_name, 'val_ft.2.npz') for ft_name in ft_names]
-  annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.B.pkl')
-  # annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.B.npz')
-  out_name = 'val.B'
-  # annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.A.pkl')
-  # out_name = 'val.A'
+  # annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.B.pkl')
+  # # annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.B.npz')
+  # out_name = 'val.B'
+  annotation_file = os.path.join(root_dir, 'split', 'val_id_caption_mask.A.pkl')
+  out_name = 'val.A'
   label_file = os.path.join(root_dir, 'label', '17.set.2.gt')
 
   # # expr_name = os.path.join(root_dir, 'ceve_expr', 'i3d_resnet200.300.1_2_3.mean.1.0')
