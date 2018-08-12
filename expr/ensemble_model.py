23911679cd012949844119d1ce8d1a34c5b3822f
Jia Chen <jiac@t470>
2018-08-11 21:54:45 -0400

    sync

--- expr/ensemble_model.py
+++ expr/ensemble_model.py
@@ -34,11 +34,12 @@ def select_best_epoch(log_dir):
 '''
 def export_avg_model_weights():
   root_dir = '/mnt/data1/jiac/trecvid2018' # neptune
-  expr_name = os.path.join(root_dir, 'generation', 'vevd_expr', 'i3d_resnet200_i3d_flow.512.512.lstm')
+  # expr_name = os.path.join(root_dir, 'generation', 'vevd_expr', 'i3d_resnet200_i3d_flow.512.512.lstm')
+  expr_name = os.path.join(root_dir, 'rank', 'vevd_expr', 'i3d_resnet200.512.512.lstm')
   log_dir = os.path.join(expr_name, 'log')
   model_cfg_file = '%s.model.json'%expr_name
   path_cfg_file = '%s.path.json'%expr_name
-  out_file = os.path.join(expr_name, 'model', 'ensemble')
+  out_file = os.path.join(expr_name, 'model', 'epoch-200')
 
   best_epoch, cider = select_best_epoch(log_dir)
   epochs = [best_epoch-10, best_epoch, best_epoch + 10]
